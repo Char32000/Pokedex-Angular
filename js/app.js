e9df92385a2e7353237b1ef5c0fec04ab3f01206
Charly Hernández <charly_hernandez@ciencias.unam.mx>
2017-03-16 15:23:10 -0600

    Añadiendo Archivos V 1.0
    Se añaden los primeros Archivos para el sitio del Pokedex usando Angular

---
+++ js/app.js
@@ -0,0 +1,49 @@
+(function() {
+
+	//aplicaciones
+    var app = angular.module('ejemplosApp', []);
+
+    app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
+
+        $scope.pokedex = {};
+        $scope.infromacionI = {};
+        $scope.mostrar = false;
+
+        /*
+        	Parte encargada de extraer el json de los datos y almacenarlos para utilizar
+        	en una tabla 
+        */
+        $http.get('http://pokeapi.co/api/v2/pokedex/2').
+        success(function(data) {
+            console.info("Datos extraidos correctamente" + data);
+
+            //copiamos el json obtenido en pokedex
+            $scope.pokedex = data;
+
+        });
+
+        /*
+        	Al momento de que el usuario de click, toma el valor poara regresar ujn json en el numero de la info y copiarla en el valor
+        */
+        $scope.unPokemon = function(valor) {
+
+            var url = 'http://pokeapi.co/api/v2/pokemon/';
+            url += valor;
+
+            //tomamos el valor del id para hacer una llamada al api y rgresar un json
+            $http.get(url).
+            success(function(datos) {
+
+                //copiamos el json obtenido en pokdex
+                //el tiempo de carga es hasta que copia los datos al array de informacionI
+                $scope.informacionI = datos;
+                $scope.mostrar = true;
+                console.info("datos cargados exitosamente");
+            });
+
+        }
+
+
+    }]);
+
+})();
